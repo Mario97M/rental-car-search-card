3432307679a1e8330c0ae36462f0f06df8a0923c
Mario Meza <mariomeza093@gmail.com>
2023-09-05 10:23:05 -0700

    Added a home page.

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <script>
 import { def } from "@vue/shared";
 import Card from "./components/Card.vue";
+import Home from "./components/Home.vue";
 
 export default {
   name: "App",
   components: {
-    Card
+    Card,
+    Home
   }
 };
 </script>
 
 <template>
   <div>
+    <Home />
     <Card />
   </div>
 </template>
